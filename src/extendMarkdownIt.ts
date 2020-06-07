299fe460508673d93797a582b4541060cb89938d
Svyatoslav Sobol <svyatoslav.sobol@gmail.com>
2020-06-07 13:37:10 +0300

    Support refs navigation in preview

--- src/extendMarkdownIt.ts
+++ src/extendMarkdownIt.ts
@@ -1,12 +1,18 @@
 import MarkdownIt from 'markdown-it';
 import markdownItRegex from 'markdown-it-regex';
 
-import { getImagePaths } from './filesCache';
+import { getImagePaths, getMarkdownPaths } from './filesCache';
 
 const imageExts = ['png', 'jpg', 'jpeg', 'svg', 'gif'];
 
-const extendMarkdownIt = (md: MarkdownIt) =>
-  md
+const getInvalidRefAnchor = (text: string) =>
+  `<a data-invalid-ref style="color: #cc0013; cursor: not-allowed;" title="Link does not exist yet. Please use cmd / ctrl + click in text editor to create a new one." href="javascript:void(0)">${text}</a>`;
+
+const getRefAnchor = (href: string, text: string) =>
+  `<a title="${text}" href="${href}">${text}</a>`;
+
+const extendMarkdownIt = (md: MarkdownIt) => {
+  return md
     .use(markdownItRegex, {
       name: 'ref-resource',
       regex: /!\[\[(.+?)\]\]/,
@@ -14,22 +20,41 @@ const extendMarkdownIt = (md: MarkdownIt) =>
         const matchLowered = match.toLowerCase();
 
         if (imageExts.some((ext) => matchLowered.includes(ext))) {
-          const imagePath = getImagePaths().find((path) => path.includes(match));
+          const imagePath = getImagePaths().find((path) =>
+            path.toLowerCase().includes(match.toLowerCase()),
+          );
 
           if (imagePath) {
             return `<img src="${imagePath}" title="${match}" />`;
           }
         }
 
-        return `<a href="javascript:void(0)">${match}</a>`;
+        const markdownPath = getMarkdownPaths().find((path) =>
+          path.toLowerCase().includes(match.toLowerCase()),
+        );
+
+        if (!markdownPath) {
+          return getInvalidRefAnchor(match);
+        }
+
+        return getRefAnchor(markdownPath, match);
       },
     })
     .use(markdownItRegex, {
       name: 'ref-document',
       regex: /\[\[(.+?)\]\]/,
       replace: (match: string) => {
-        return `<a href="javascript:void(0)">${match}</a>`;
+        const markdownPath = getMarkdownPaths().find((path) =>
+          path.toLowerCase().includes(match.toLowerCase()),
+        );
+
+        if (!markdownPath) {
+          return getInvalidRefAnchor(match);
+        }
+
+        return getRefAnchor(markdownPath, match);
       },
     });
+};
 
 export default extendMarkdownIt;
--- src/filesCache.ts
+++ src/filesCache.ts
@@ -2,10 +2,16 @@ import { ExtensionContext, workspace } from 'vscode';
 
 let imagePaths: string[] = [];
 
+let markdownPaths: string[] = [];
+
 export const getImagePaths = () => imagePaths;
 
-export const activate = async (_: ExtensionContext) => {
-  const uris = await workspace.findFiles('**/*.{png,jpg,jpeg,svg,gif}');
+export const getMarkdownPaths = () => markdownPaths;
 
-  imagePaths = uris.map((uri) => uri.fsPath);
+export const activate = async (_: ExtensionContext) => {
+  const imageUris = await workspace.findFiles('**/*.{png,jpg,jpeg,svg,gif}');
+  const markdownUris = await workspace.findFiles('**/*.md');
+
+  imagePaths = imageUris.map((uri) => uri.fsPath);
+  markdownPaths = markdownUris.map((uri) => uri.fsPath);
 };
