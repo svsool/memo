d76ef273071070f8c665da1ca2d6d10aaa9ea838
Svyatoslav Sobol <svyatoslav.sobol@gmail.com>
2020-06-09 00:37:42 +0300

    Consider already opened text documents on using openRandomNote

--- src/commands/openRandomNote.ts
+++ src/commands/openRandomNote.ts
@@ -1,9 +1,11 @@
-import { commands } from 'vscode';
+import { commands, workspace } from 'vscode';
 
 import { getMarkdownUris } from '../utils';
 
 const openRandomNote = () => {
-  const markdownUris = getMarkdownUris();
+  const { textDocuments } = workspace;
+  const openedFileNames = textDocuments.map((d) => d.fileName);
+  const markdownUris = getMarkdownUris().filter(({ fsPath }) => !openedFileNames.includes(fsPath));
   const randomUriIndex = Math.round(Math.random() * markdownUris.length);
   const randomUri = markdownUris[randomUriIndex];
 
