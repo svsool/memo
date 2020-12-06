4d14d09d7a26792808388cfd2c9640e36ad681da
Svyat Sobol <svyatoslav.sobol@gmail.com>
2020-12-06 17:06:22 +0200

    chore: Add new version teaser

--- src/features/newVersionNotifier.ts
+++ src/features/newVersionNotifier.ts
@@ -4,6 +4,8 @@ import { window, commands, Uri, ExtensionContext } from 'vscode';
 
 // Add new version teasers for showing notification on extension update if needed
 const teasers: { [key: string]: string } = {
+  '0.3.1':
+    'Memo v0.3.1! Links format "absolute" deprecated, please consider changing it to "long" in the settings.',
   '0.1.11': 'Memo v0.1.11! New "Open daily note" command added.',
   '0.1.10': 'Memo v0.1.10! Links rename, opening links in the default app and more.',
 };
