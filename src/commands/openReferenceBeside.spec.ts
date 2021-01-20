1050a9416023620d1a0905c9b31fb2d3baed5fee
ez4gis <ez4gis@gmail.com>
2021-01-20 00:36:58 -0800

    fix: Fix tests for openReferenceBeside

--- src/commands/openReferenceBeside.spec.ts
+++ src/commands/openReferenceBeside.spec.ts
@@ -7,13 +7,14 @@ import {
   openTextDocument,
   rndName,
   toPlainObject,
+  waitForExpect,
 } from '../test/testUtils';
 
 describe('openReferenceBeside command', () => {
   beforeEach(closeEditorsAndCleanWorkspace);
   afterEach(closeEditorsAndCleanWorkspace);
 
-  it('should call open command-line tool when editor selection is within the reference', async () => {
+  it('should execute vscode.open when editor selection is within the reference', async () => {
     const executeCommandSpy = jest.spyOn(commands, 'executeCommand');
 
     const name0 = rndName();
@@ -39,14 +40,14 @@ describe('openReferenceBeside command', () => {
           path: expect.toEndWith(`${name0}.md`),
           scheme: 'file',
         }),
-        -2, // ViewColumn.Beside
+        ViewColumn.Beside,
       ],
     ]);
 
     executeCommandSpy.mockRestore();
   });
 
-  it('should NOT call open command-line tool when editor selection is outside of the reference', async () => {
+  it('should NOT execute vscode.open when editor selection is outside of the reference', async () => {
     const executeCommandSpy = jest.spyOn(commands, 'executeCommand');
 
     const name0 = rndName();
@@ -74,22 +75,19 @@ describe('openReferenceBeside command', () => {
     const name1 = rndName();
 
     await createFile(`${name0}.md`);
-    await createFile(`${name1}.md`, `[[${name0}.md]]`);
+    await createFile(`${name1}.md`, `[[${name0}]]`);
 
     const doc = await openTextDocument(`${name1}.md`);
     const editor = await window.showTextDocument(doc);
+
     editor.selection = new Selection(0, 2, 0, 2);
+
     expect(window.activeTextEditor === editor).toBeTrue();
     expect(window.activeTextEditor!.viewColumn === ViewColumn.One).toBeTrue();
 
     await openReferenceBeside();
 
-    console.log('visibleTextEditors.length', window.visibleTextEditors.length);
-    console.log('activeTextEditor.viewColumn', window.activeTextEditor?.viewColumn);
-    console.log('activeTextEditor.document.filename', window.activeTextEditor?.document.fileName);
-    // TODO: openReferenceBeside appears to work in vscode,
-    //       but the following checks in testing script always fail!:(
-    expect(window.visibleTextEditors.length === 2).toBeTrue();
+    await waitForExpect(() => expect(window.visibleTextEditors.length === 2).toBeTrue());
     expect(window.activeTextEditor!.viewColumn === ViewColumn.Two).toBeTrue();
   });
 });
