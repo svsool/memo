b2eb68758f7a07286bf165a9cfc87fe8411e835b
Svyat Sobol <svyatoslav.sobol@gmail.com>
2021-03-29 00:02:53 +0300

    chore(deps-dev): Update husky

--- .huskyrc.js
+++
@@ -1,8 +0,0 @@
-const disableLinters = Number(process.env.DISABLE_MEMO_HOOKS) === 1;
-
-module.exports = {
-  hooks: {
-    'pre-commit': disableLinters ? undefined : 'lint-staged',
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-  },
-};
