284d06f3f4eb673e47f767aa212c2e26b277e839
Svyat Sobol <svyatoslav.sobol@gmail.com>
2022-01-07 23:53:35 +0200

    feat: Use rigrep to speed up refs rename (#286)

---
+++ src/utils/search.ts
@@ -0,0 +1,84 @@
+import fs from 'fs-extra';
+import * as vscode from 'vscode';
+import { execa, ExecaError } from 'execa';
+import path from 'path';
+
+import { escapeForRegExp } from './utils';
+import logger from '../logger';
+
+let ripgrepPathGlobal: string | undefined | false;
+
+const findRipgrepPath = async (): Promise<string | undefined> => {
+  if (ripgrepPathGlobal) {
+    return ripgrepPathGlobal;
+  }
+
+  const name = /^win/.test(process.platform) ? 'rg.exe' : 'rg';
+  const vscodePath = vscode.env.appRoot;
+
+  const ripgrepPaths = [
+    name,
+    path.join(vscodePath, `node_modules.asar.unpacked/vscode-ripgrep/bin/${name}`),
+    path.join(vscodePath, `node_modules/vscode-ripgrep/bin/${name}`),
+  ];
+
+  for (const ripgrepPath of ripgrepPaths) {
+    try {
+      logger.info(`Trying ripgrep at path "${ripgrepPath}"`);
+
+      await execa(ripgrepPath, ['--version']);
+
+      ripgrepPathGlobal = ripgrepPath;
+
+      logger.info(`Ripgrep detected at path "${ripgrepPath}"`);
+
+      return ripgrepPath;
+    } catch (e) {
+      logger.warn(`No rigrep bin found at path "${ripgrepPath}"`);
+    }
+  }
+
+  logger.warn('No rigrep bin detected!');
+
+  ripgrepPathGlobal = false;
+
+  return;
+};
+
+export const search = async (refs: string[], dirPath: string = '.'): Promise<string[]> => {
+  const ripgrepPath = await findRipgrepPath();
+
+  if (!ripgrepPath || !refs.length) {
+    return [];
+  }
+
+  try {
+    const res = await execa(ripgrepPath, [
+      '-l',
+      '--ignore-case',
+      '--color',
+      'never',
+      '-g',
+      `*.md`,
+      `(${refs.map(escapeForRegExp).join('|')})`,
+      dirPath,
+    ]);
+
+    const paths = res.stdout
+      .toString()
+      .split(/\r?\n/g)
+      .filter((path) => path);
+
+    if (paths.length && (await fs.pathExists(paths[0]))) {
+      return paths;
+    }
+  } catch (e) {
+    if ((e as ExecaError).exitCode !== 1) {
+      console.error(e);
+    }
+  }
+
+  return [];
+};
+
+export default search;
